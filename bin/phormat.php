92c01cd127a33f96a5864032e6f20a57362e8bde
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-30 14:51:01 -0500

    Update phormat.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bin/phormat.php
+++ bin/phormat.php
@@ -11,27 +11,21 @@ use Ghostwriter\Phormat\Phormat;
 use const DIRECTORY_SEPARATOR;
 use const STDERR;
 
-use function dirname;
-use function error_reporting;
-use function file_exists;
-use function fwrite;
-use function set_error_handler;
-
 (
     static function (string $autoloader): void {
-        if (! file_exists($autoloader)) {
+        if (! \file_exists($autoloader)) {
             $message = '[ERROR]Cannot locate "' . $autoloader . '"\\n please run "composer install"\\n';
 
-            fwrite(STDERR, $message);
+            \fwrite(STDERR, $message);
 
             exit();
         }
 
         require $autoloader;
 
-        set_error_handler(
+        \set_error_handler(
             static function (int $severity, string $message, string $file, int $line): void {
-                if ((error_reporting() & $severity) === 0) {
+                if ((\error_reporting() & $severity) === 0) {
                     return;
                 }
 
@@ -44,4 +38,4 @@ use function set_error_handler;
          */
         Phormat::new()->run();
     }
-)($_composer_autoload_path ?? dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php');
+)($_composer_autoload_path ?? \dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php');
