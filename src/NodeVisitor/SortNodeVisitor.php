a93a17633a8ec4f61e5b0a784b48eb5938605e96
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-10 10:05:56 -0600

    Add SortNodeVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/NodeVisitor/SortNodeVisitor.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Phormat\NodeVisitor;
+
+use PhpParser\NodeVisitorAbstract;
+
+final class SortNodeVisitor extends NodeVisitorAbstract
+{
+    //    public function format(Node ...$nodes): void
+    //    {
+    //        foreach ($nodes as $node) {
+    //            $this->sort($node);
+    //        }
+    //    }
+}
