c5c9dee29f69621e7c19cd2b47ef995aece8ff00
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-10 10:06:00 -0600

    Add VisitorMustImplementNodeVisitorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/VisitorMustImplementNodeVisitorInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Phormat;
+
+use InvalidArgumentException;
+
+final class VisitorMustImplementNodeVisitorInterface extends InvalidArgumentException
+{
+}
