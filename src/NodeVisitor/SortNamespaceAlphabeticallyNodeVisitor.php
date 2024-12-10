7ceb77d1f160b53cffb1dfe0039b6003ca0de7e2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-10 10:05:56 -0600

    Add SortNamespaceAlphabeticallyNodeVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/NodeVisitor/SortNamespaceAlphabeticallyNodeVisitor.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Phormat\NodeVisitor;
+
+use PhpParser\NodeVisitorAbstract;
+
+final class SortNamespaceAlphabeticallyNodeVisitor extends NodeVisitorAbstract
+    //final class SortNamespaceAlphabeticallyNodeVisitor extends AbstractNodeVisitor implements FormatterInterface
+{
+    //    public function format(Node $node): null|array|int|Node
+    //    {
+    //        return $this->sort($node);
+    //    }
+    //
+    //    public function matches(Node $node): bool
+    //    {
+    //        return $node instanceof Namespace_;
+    //    }
+}
