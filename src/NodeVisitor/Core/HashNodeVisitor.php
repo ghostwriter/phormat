203543a0c7d026a81164ceadd0d5487f29f2d0da
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-10 10:05:54 -0600

    Add HashNodeVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/NodeVisitor/Core/HashNodeVisitor.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Phormat\NodeVisitor\Core;
+
+use Override;
+use PhpParser\Node;
+use PhpParser\NodeVisitorAbstract;
+
+final class HashNodeVisitor extends NodeVisitorAbstract
+{
+    #[Override]
+    public function enterNode(Node $node): ?Node
+    {
+        $node->setAttribute(self::class, \spl_object_hash($node));
+        return $node;
+    }
+}
