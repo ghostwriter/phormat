6d3a30b95453ae38e34f2b0378b0a12cf57be4b1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-10 10:05:53 -0600

    Add SkippedPathMustBeStringException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/SkippedPathMustBeStringException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Phormat\Exception;
+
+use InvalidArgumentException;
+
+final class SkippedPathMustBeStringException extends InvalidArgumentException
+{
+}
