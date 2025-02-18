84d1c6f37ac85a012186d26ba5cef7c6ed94f939
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:49 -0600

    Update SkippedPathDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/SkippedPathDoesNotExistException.php
+++ src/Exception/SkippedPathDoesNotExistException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use InvalidArgumentException;
 
-final class SkippedPathDoesNotExistException extends InvalidArgumentException
-{
-}
+final class SkippedPathDoesNotExistException extends InvalidArgumentException {}
