dde52d0b94be4749abe3a4530f37e1e4ac3b0cf2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:50 -0600

    Update AlternativeIdNodeVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/NodeVisitor/AlternativeIdNodeVisitor.php
+++ src/NodeVisitor/AlternativeIdNodeVisitor.php
@@ -6,13 +6,15 @@ namespace Ghostwriter\Phormat\NodeVisitor;
 
 use Override;
 
+use function spl_object_hash;
+
 final class AlternativeIdNodeVisitor extends AbstractNodeVisitor
 {
     #[Override]
     public function beforeTraverse(array $nodes): ?array
     {
         foreach ($nodes as $node) {
-            $node->setAttribute(self::class, \spl_object_hash($node));
+            $node->setAttribute(self::class, spl_object_hash($node));
         }
 
         return $nodes;
