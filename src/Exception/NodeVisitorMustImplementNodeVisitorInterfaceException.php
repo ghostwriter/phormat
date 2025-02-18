41863f86aecb608b6707287417e62b9cf7b4b5c1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:49 -0600

    Update NodeVisitorMustImplementNodeVisitorInterfaceException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/NodeVisitorMustImplementNodeVisitorInterfaceException.php
+++ src/Exception/NodeVisitorMustImplementNodeVisitorInterfaceException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use InvalidArgumentException;
 
-final class NodeVisitorMustImplementNodeVisitorInterfaceException extends InvalidArgumentException
-{
-}
+final class NodeVisitorMustImplementNodeVisitorInterfaceException extends InvalidArgumentException {}
