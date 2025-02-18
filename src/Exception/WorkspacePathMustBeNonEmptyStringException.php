7f1e47752b2ea9239f98748c324a60e1511d6532
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:50 -0600

    Update WorkspacePathMustBeNonEmptyStringException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/WorkspacePathMustBeNonEmptyStringException.php
+++ src/Exception/WorkspacePathMustBeNonEmptyStringException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use RuntimeException;
 
-final class WorkspacePathMustBeNonEmptyStringException extends RuntimeException
-{
-}
+final class WorkspacePathMustBeNonEmptyStringException extends RuntimeException {}
