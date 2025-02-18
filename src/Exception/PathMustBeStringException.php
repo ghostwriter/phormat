1140c76532c3dc37ecc3d4b4a633dd4217c0f0f8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:49 -0600

    Update PathMustBeStringException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/PathMustBeStringException.php
+++ src/Exception/PathMustBeStringException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use InvalidArgumentException;
 
-final class PathMustBeStringException extends InvalidArgumentException
-{
-}
+final class PathMustBeStringException extends InvalidArgumentException {}
