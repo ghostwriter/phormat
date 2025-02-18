fb103dbd5a17d9cad5e1a111f9363d3512d7ddbb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:48 -0600

    Update FileDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FileDoesNotExistException.php
+++ src/Exception/FileDoesNotExistException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use InvalidArgumentException;
 
-final class FileDoesNotExistException extends InvalidArgumentException
-{
-}
+final class FileDoesNotExistException extends InvalidArgumentException {}
