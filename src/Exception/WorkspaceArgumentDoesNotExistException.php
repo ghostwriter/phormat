8c124248978a4e435cd64b51406c5d2493524301
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:49 -0600

    Update WorkspaceArgumentDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/WorkspaceArgumentDoesNotExistException.php
+++ src/Exception/WorkspaceArgumentDoesNotExistException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use RuntimeException;
 
-final class WorkspaceArgumentDoesNotExistException extends RuntimeException
-{
-}
+final class WorkspaceArgumentDoesNotExistException extends RuntimeException {}
