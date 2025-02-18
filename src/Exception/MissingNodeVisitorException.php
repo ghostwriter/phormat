6245d6903a31ab18ce2d15586998a024080db528
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:49 -0600

    Update MissingNodeVisitorException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/MissingNodeVisitorException.php
+++ src/Exception/MissingNodeVisitorException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use InvalidArgumentException;
 
-final class MissingNodeVisitorException extends InvalidArgumentException
-{
-}
+final class MissingNodeVisitorException extends InvalidArgumentException {}
