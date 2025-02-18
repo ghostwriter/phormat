775bf252037aafe5d2f4af0a6fe2df93b1ec6916
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:48 -0600

    Update FileNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FileNotFoundException.php
+++ src/Exception/FileNotFoundException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use RuntimeException;
 
-final class FileNotFoundException extends RuntimeException
-{
-}
+final class FileNotFoundException extends RuntimeException {}
