864d10dabf292d176c37202fcb8c1555500c7354
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 16:34:48 -0600

    Update DirectoryDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/DirectoryDoesNotExistException.php
+++ src/Exception/DirectoryDoesNotExistException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Phormat\Exception;
 
 use InvalidArgumentException;
 
-final class DirectoryDoesNotExistException extends InvalidArgumentException
-{
-}
+final class DirectoryDoesNotExistException extends InvalidArgumentException {}
